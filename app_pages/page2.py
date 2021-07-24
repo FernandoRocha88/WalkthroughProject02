cee489bdeb6f9c8ac8f94e71a533bb8d3fece22a
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-07-24 15:22:49 +0000

    udpate

--- app_pages/page2.py
+++ app_pages/page2.py
@@ -3,3 +3,23 @@ import streamlit as st
 def page2_body():
     st.write("### User Interface")
     st.write("* Page with prospect inputs and predictive analysis")
+
+
+    # inputs
+
+
+    # clf
+    # predict
+    # predict_proba
+
+
+    # tenure
+    # if predict == 1
+    # .predict
+
+
+    # cluster
+    # get inputs
+    # predict prosecpt cluster
+    # show cluster profile
+    # show options for each variable in the profile
