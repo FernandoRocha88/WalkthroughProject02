9baa47aadca18c2a023480806ca0dca63d433f5f
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-07-24 14:14:22 +0000

    update

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
 from app_pages.page1 import page1_body
-
+from app_pages.page2 import page2_body
+from app_pages.page3 import page3_body
+from app_pages.page4 import page4_body
+from app_pages.page5 import page5_body
 app = MultiPage() # Create an instance of the app 
-
 
 
 # Add all your applications (pages) here
 app.add_page("Customer Churn Study", page1_body)
-# app.add_page("Change Metadata", metadata.app)
-# app.add_page("Machine Learning", machine_learning.app)
-# app.add_page("Data Analysis",data_visualize.app)
-# app.add_page("Y-Parameter Optimization",redundant.app)
+app.add_page("User Interface", page2_body)
+app.add_page("ML: Prospect Churn", page3_body)
+app.add_page("ML: Prospect Tenure", page4_body)
+app.add_page("ML: Cluster Base", page5_body)
 
-# The main app
-app.run()
+app.run() # Run the  app
