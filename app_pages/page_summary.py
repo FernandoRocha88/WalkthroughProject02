531d7788bc2b00ff830653e178ac8bf5811a25e2
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-08-02 13:51:59 +0000

    update

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -8,9 +8,9 @@ def page_summary_body():
     st.write("### Quick Project Summary")
     st.write(
         f"* A **churned** customer is a user who has stopped using your product/service. "
-        f"This customer, has a **tenure** level, which is the number of months this person" 
+        f"This customer, has a **tenure** level, which is the number of months this person " 
         f"has used our product/service. \n"
-        f"* We are interested to use an AI system to predict: \n"
+        f"* We are interested to build an AI system to predict: \n"
         f"  * If a prospect will churn, if so, when? \n"
         f"  * From which group this prospect will belong, and based on that, "
         f"present potential factors that could mantain/bring the prospect to "
@@ -21,6 +21,6 @@ def page_summary_body():
     st.image(project_snapshot)
 
     st.write(
-        f"* For additional information, please visit and read the "
+        f"* For additional information, please **visit and read** the "
         f"[Project README file](https://github.com/FernandoRocha88/WalkthroughProject02/blob/main/README.md).")
     
