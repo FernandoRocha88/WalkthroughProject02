3d938cbf5b579a935c553c45aff436d9da2dde66
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-08-09 19:17:57 +0000

    update

--- app_pages/page_customer_base_churn.py
+++ app_pages/page_customer_base_churn.py
@@ -5,70 +5,79 @@ from src.correlation_analysis import CalculateCorrAndPPS, DisplayCorrAndPPS
 def page_customer_base_churn_body():
     st.write("### Customer Base Churn Study")
     st.write(
-        f"* It answers biz requirement 1 \n\n"
-        f"As a customer I am interested to understand the patterns from my customer base, "
+        f"#### As a customer I am interested to understand the patterns from my customer base, "
         f"so I can better manage churn levels.")
 
 
     # load data
     df = load_telco_data()
 
-    # Check collected data
-    if st.checkbox("Check Collected Data"):
-        check_data(df)
-        
+    # hard copied from data visualization notebook
+    vars_to_study = ['Contract', 'InternetService',
+                    'OnlineSecurity', 'PaymentMethod',
+                    'TechSupport', 'tenure']
 
-
-    # correlation and pps analysis
-    if st.checkbox("Conduct Correlation and PPS Analysis on collected Data"):
-        correlation_study(df)
-        
-
-
-    # how is churn level across tenure
-    # if st.checkbox("Churn level across tenure"):
-    #     import matplotlib.pyplot as plt
-    #     import seaborn as sns
-    #     fig, axes = plt.subplots(nrows=1, ncols=2)
-    #     sns.boxplot(data=df,x='Churn', y='tenure', ax=axes[0])
-    #     sns.histplot(data=df, x='tenure', hue='Churn', kde=True, ax=axes[1])
-    #     st.pyplot(fig)
-
-
-
-    # how is churn levels across main variables at clf
-    if st.checkbox("Churn across main variables at clf"):
-        churn_across_features(df)
-        
-
-
-def check_data(df):
-    st.write("#### Snapshot: Collected Data")
-    st.write(f"Dataset shape: {df.shape}")
-    st.write(df)
+    # inspect collected data
+    if st.checkbox("Inspect Customer Base"):
+        inspect_data(df)
     st.write("---")
 
+    # Correlation Study Summary
+    st.write(
+        f"* A correlation study was conducted in the notebook to better understand how "
+        f"the variables are correlated to Churn levels. \n"
+        f"The most correlated variable are: **{vars_to_study}**")
+
+    df_eda = df.filter(vars_to_study + ['Churn'])
+        
+
+    # Individual plots per variable
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    if st.checkbox("Churn Levels per Variable Distribution"):
+        churn_level_per_variable(df_eda)
+        
+    # Parallel plot
+    if st.checkbox("Parallel Plot"):
+        st.write("asasd")
+        # churn_across_features(df[vars_to_study])
+        
 
 
-def correlation_study(df):
-    from feature_engine.encoding import OrdinalEncoder
-    encoder = OrdinalEncoder(encoding_method='ordered', variables = df.select_dtypes(include=['object']).columns.to_list())
-    df_processed = encoder.fit_transform(df, df['Churn'])
-    # st.write(df_processed)
-
-    df_corr_pearson, df_corr_spearman, pps_matrix = CalculateCorrAndPPS(df_processed)
-    DisplayCorrAndPPS(
-        df_corr_pearson, df_corr_spearman, pps_matrix,
-        CorrThreshold=0.2, PPS_Threshold=0.2)
+def inspect_data(df):
+    st.write(
+        f"For dataset explanation, visit project "
+        f"[repo](https://github.com/FernandoRocha88/WalkthroughProject02/blob/main/README.md)")
+    st.write(f"Dataset shape: {df.shape}")
+    st.write(df)
+    
 
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+def plot_categorical(df, col, target_var):
 
-def churn_across_features(df):
-    import matplotlib.pyplot as plt
-    import plotly.express as px
-    from feature_engine.encoding import OrdinalEncoder
+  plt.figure(figsize=(12, 5))
+  sns.countplot(data=df, x=col, hue=target_var,order = df[col].value_counts().index)
+  plt.xticks(rotation=90) 
+  plt.title(f"{col}", fontsize=20,y=1.05)        
+  st.pyplot()
 
-    encoder = OrdinalEncoder(encoding_method='ordered', variables = df.select_dtypes(include=['object']).columns.to_list())
-    df_processed = encoder.fit_transform(df, df['Churn'])
-    fig = px.parallel_coordinates(df_processed[['Churn','Contract','tenure']], color="Churn")
-    st.plotly_chart(fig)
+def plot_numerical(df, col, target_var):
+  plt.figure(figsize=(8, 5))
+  sns.histplot(data=df, x=col, hue=target_var, kde=True,element="step") 
+  plt.title(f"{col}", fontsize=20,y=1.05)
+  st.pyplot()
+
+
+def churn_level_per_variable(df_eda):
+    target_var = 'Churn'
+
+    for col in df_eda.drop([target_var], axis=1).columns.to_list():
+        if df_eda[col].dtype == 'object':
+            plot_categorical(df_eda, col, target_var)
+        else:
+            plot_numerical(df_eda, col, target_var)
+
+
+
+
