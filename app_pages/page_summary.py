e893ed627f5adb4155d6314942e8f251c84f3915
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-08-12 11:16:00 +0000

    update

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -12,13 +12,20 @@ def page_summary_body():
         f"[Project README file](https://github.com/FernandoRocha88/WalkthroughProject02/blob/main/README.md).")
     
 
-    st.write(
-        f"* A **churned** customer is a user who has stopped using your product/service. "
-        f"This customer, has a **tenure** level, which is the number of months this person " 
-        f"has used our product/service. \n"
-        f"* We are interested to build an AI system to predict: \n"
-        f"  * If a prospect will churn, if so, when? \n"
-        f"  * From which group this prospect will belong, and based on that, "
+    st.info(
+        f"**Project Terms & Jargons**\n"
+        f"* A **customer** is a person who uses/pays your service or product.\n"
+        f"* A **prospect** is a potential customer.\n"
+        f"* A **churned** customer is a user who has stopped using your product/service.\n "
+        f"* This customer, has a **tenure** level, which is the number of months this person " 
+        f"has used our product/service. \n")
+
+    st.success(
+
+        f"We are interested to build an AI system to:\n"
+        f"* 1 - Study Customer Base Churn.\n"
+        f"* 2 - Predict if a prospect will churn, if so, when?"
+        f"In addition, tell from which group this prospect will belong, and based on that, "
         f"present potential factors that could mantain/bring the prospect to "
         f"a non-churnable cluster."
         )
